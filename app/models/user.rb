10ad1da76dd32a8dc2526b7b8cd94f0527d22e86
t-hirata74 <t.hirata.0463@gmail.com>
2023-04-16 19:28:49 +0900

    deviseの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, # 認証
+         :registerable, # 新規登録
+         :recoverable, # パスワードリセット
+         :rememberable, # ログイン状態を保持
+         :validatable, # バリデーション
+         :confirmable, # メールアドレス認証
+         :lockable, # パスワード複数間違いでアカウントロック
+         :timeoutable, # ログイン保持機能
+         :trackable # ログイン時のipなどを記録
 end
