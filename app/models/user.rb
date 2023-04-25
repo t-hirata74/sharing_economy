085c089cc61fe7c18740de1f070cd0c6d68298ff
t-hirata74 <t.hirata.0463@gmail.com>
2023-04-25 23:54:00 +0900

    genderのenumを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,16 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :gender, presence: true
+
+  enum gender: {
+    unanswered: 0,
+    male: 1,
+    demale: 2
+  }
+
+  class << self
+    def genders_i18n
+      I18n.t("enums.user.gender")
+    end
+  end
 end
