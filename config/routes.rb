05790c91108a1140cd54aaeb1e27363666dc29df
t-hirata74 <t.hirata.0463@gmail.com>
2023-05-09 22:54:13 +0900

    routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+  resource :user_information
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
