7ac42de0b481f4e7363a6568a5e05c691909c9cc
t-hirata74 <t.hirata.0463@gmail.com>
2023-05-09 23:13:41 +0900

    newアクションの実装

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -1,7 +1,8 @@
 class UserInformationsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
+    @user_information = current_user.build_user_information
   end
 
   def create
