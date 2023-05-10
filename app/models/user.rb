2cfb577e6d1519d59b77bff8ae744293c9cfb3e3
t-hirata74 <t.hirata.0463@gmail.com>
2023-05-10 23:47:00 +0900

    アソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ApplicationRecord
   before_validation :skip_confirmation!, if: :new_record?
 
   has_one :user_information, dependent: :destroy
-
+  has_one :user_mobile_phone, dependent: :destroy
+  
   enum gender: {
     unanswered: 0,
     male: 1,
