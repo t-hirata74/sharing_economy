1bf96e42d36962a74c14a8f75964fff854df5648
t-hirata74 <t.hirata.0463@gmail.com>
2023-05-16 23:07:25 +0900

    routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+  devise_scope :user do
+    get "users/registrations/complate" => "users/registrations#complete"
+  end
+  
   resource :user_information
   resource :user_mobile_phone, only: %i[new create] do
     collection do
