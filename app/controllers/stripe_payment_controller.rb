ad72402d50e9a82b7507d87c95d6afd8d97324dd
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-09 00:10:44 +0900

    docker-compose run --rm web rails g controller StripePayment index new create edit update destory

---
+++ app/controllers/stripe_payment_controller.rb
@@ -0,0 +1,19 @@
+class StripePaymentController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destory
+  end
+end
