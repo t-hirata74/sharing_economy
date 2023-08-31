f708da725d6ede157a9f4eb2508b1e8bdf4a05ef
t-hirata74 <t.hirata.0463@gmail.com>
2023-08-31 23:56:37 +0900

    destoryアクションの実装

--- app/controllers/mypage/favorites_controller.rb
+++ app/controllers/mypage/favorites_controller.rb
@@ -2,6 +2,14 @@ class Mypage::FavoritesController < ApplicationController
   layout "mypage"
   before_action :authenticate_user!
 
-  def index destroy
+  def index
+  end
+
+  def destroy
+    favorite = current_user.favorites.find(params[:id])
+
+    favorite.destroy
+
+    redirect_to mypage_favorites_path, notice: "いいねを削除しました"
   end
 end
