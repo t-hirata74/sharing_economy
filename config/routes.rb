8d7001eda32a576451ff767ef46a5366cd369d06
t-hirata74 <t.hirata.0463@gmail.com>
2023-09-01 00:33:23 +0900

    routingの実装

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,8 @@ Rails.application.routes.draw do
   get "mypage" => "mypage#index"
   namespace :mypage do
     resources :favorites, only: %i[index destroy]
+    resources :items, only: %i[index]
   end
-  
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
